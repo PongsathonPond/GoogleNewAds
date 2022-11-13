b6648ab274d8f78b907cc1f03ebccb0be29f3047
pongsathon.l <pongsathon.l@touchtechnologies.co.th>
2022-11-14 03:56:26 +0700

    tss

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.app')
-
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
