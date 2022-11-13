ac6fcf0257cc21c0b73785f5c753bf5c03ed44c4
PongsathonPond <93896876+PongsathonPond@users.noreply.github.com>
2022-11-14 02:46:24 +0700

    Update index.blade.php

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -1,5 +1,4 @@
 @extends('layouts.app')
-@section('title',"$setting->meta_title")
 @section('meta_description',"$setting->meta_description")
 @section('meta_keyword',"$setting->meta_keyword")
 @section('content')
