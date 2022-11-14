352da93cce4ce7df24b9e618e186420f62b351e3
pongsathon <pongsathon.la@rmuti.ac.th>
2022-11-14 16:16:19 +0700

    tst

--- routes/web.php
+++ routes/web.php
@@ -16,29 +16,32 @@ Route::get('/tutorial/{category_slug}/{post_slug}', [App\Http\Controllers\Fronte
 Route::post('comments', [App\Http\Controllers\Frontend\CommentController::class, 'store']);
 Route::post('delete-comment', [App\Http\Controllers\Frontend\CommentController::class, 'destroy']);
 
-Route::get('admin/dashboard', [App\Http\Controllers\Admin\DashboardController::class, 'index']);
+Route::prefix('admin')->middleware(['auth', 'isAdmin'])->group(function () {
+    Route::get('/dashboard', [App\Http\Controllers\Admin\DashboardController::class, 'index']);
 
-//category
-Route::get('admin/category', [App\Http\Controllers\Admin\CategoryController::class, 'index']);
-Route::get('admin/add-category', [App\Http\Controllers\Admin\CategoryController::class, 'create']);
-Route::post('admin/add-category', [App\Http\Controllers\Admin\CategoryController::class, 'store']);
-Route::get('admin/edit-category/{category_id}', [App\Http\Controllers\Admin\CategoryController::class, 'edit']);
-Route::put('admin/update-category/{category_id}', [App\Http\Controllers\Admin\CategoryController::class, 'update']);
+    //category
+    Route::get('category', [App\Http\Controllers\Admin\CategoryController::class, 'index']);
+    Route::get('add-category', [App\Http\Controllers\Admin\CategoryController::class, 'create']);
+    Route::post('add-category', [App\Http\Controllers\Admin\CategoryController::class, 'store']);
+    Route::get('edit-category/{category_id}', [App\Http\Controllers\Admin\CategoryController::class, 'edit']);
+    Route::put('update-category/{category_id}', [App\Http\Controllers\Admin\CategoryController::class, 'update']);
 //    Route::get('delete-category/{category_id}',[App\Http\Controllers\Admin\CategoryController::class,'destroy']);
-Route::post('admin/delete-category', [App\Http\Controllers\Admin\CategoryController::class, 'destroy']);
+    Route::post('delete-category', [App\Http\Controllers\Admin\CategoryController::class, 'destroy']);
 
-//posts
-Route::get('admin/posts', [App\Http\Controllers\Admin\PostController::class, 'index']);
-Route::get('admin/add-post', [App\Http\Controllers\Admin\PostController::class, 'create']);
-Route::post('admin/add-post', [App\Http\Controllers\Admin\PostController::class, 'store']);
-Route::get('admin/post/{post_id}', [App\Http\Controllers\Admin\PostController::class, 'edit']);
-Route::put('admin/update-post/{post_id}', [App\Http\Controllers\Admin\PostController::class, 'update']);
-Route::get('admin/delete-post/{post_id}', [App\Http\Controllers\Admin\PostController::class, 'destroy']);
+    //posts
+    Route::get('posts', [App\Http\Controllers\Admin\PostController::class, 'index']);
+    Route::get('add-post', [App\Http\Controllers\Admin\PostController::class, 'create']);
+    Route::post('add-post', [App\Http\Controllers\Admin\PostController::class, 'store']);
+    Route::get('post/{post_id}', [App\Http\Controllers\Admin\PostController::class, 'edit']);
+    Route::put('update-post/{post_id}', [App\Http\Controllers\Admin\PostController::class, 'update']);
+    Route::get('delete-post/{post_id}', [App\Http\Controllers\Admin\PostController::class, 'destroy']);
 
-//users
-Route::get('admin/users', [App\Http\Controllers\Admin\UserController::class, 'index']);
-Route::get('admin/user/{user_id}', [App\Http\Controllers\Admin\UserController::class, 'edit']);
-Route::put('admin/update-user/{user_id}', [App\Http\Controllers\Admin\UserController::class, 'update']);
+    //users
+    Route::get('users', [App\Http\Controllers\Admin\UserController::class, 'index']);
+    Route::get('user/{user_id}', [App\Http\Controllers\Admin\UserController::class, 'edit']);
+    Route::put('update-user/{user_id}', [App\Http\Controllers\Admin\UserController::class, 'update']);
 
-Route::get('admin/settings', [App\Http\Controllers\Admin\SettingController::class, 'index']);
-Route::post('admin/settings', [App\Http\Controllers\Admin\SettingController::class, 'savedata']);
+    Route::get('settings', [App\Http\Controllers\Admin\SettingController::class, 'index']);
+    Route::post('settings', [App\Http\Controllers\Admin\SettingController::class, 'savedata']);
+
+});
