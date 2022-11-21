72a415cd73e59de89e263512c9bc3d59254abc31
pongsathon <pongsathon.la@rmuti.ac.th>
2022-11-21 19:20:36 +0700

    test

--- routes/web.php
+++ routes/web.php
@@ -1,48 +1,18 @@
 <?php
 
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Auth::routes();
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-//frontend
-Route::get('/', [App\Http\Controllers\Frontend\FrontendController::class, 'index']);
-Route::get('tutorial/{category_slug}', [App\Http\Controllers\Frontend\FrontendController::class, 'viewCategoryPost']);
-Route::get('/tutorial/{category_slug}/{post_slug}', [App\Http\Controllers\Frontend\FrontendController::class, 'viewPost']);
-
-// Comment System
-Route::post('comments', [App\Http\Controllers\Frontend\CommentController::class, 'store']);
-Route::post('delete-comment', [App\Http\Controllers\Frontend\CommentController::class, 'destroy']);
-
-
-
-Route::prefix('admin')->group(function () {
-    Route::get('/dashboard', [App\Http\Controllers\Admin\DashboardController::class, 'index']);
-    //category
-    Route::get('category', [App\Http\Controllers\Admin\CategoryController::class, 'index']);
-    Route::get('add-category', [App\Http\Controllers\Admin\CategoryController::class, 'create']);
-    Route::post('add-category', [App\Http\Controllers\Admin\CategoryController::class, 'store']);
-    Route::get('edit-category/{category_id}', [App\Http\Controllers\Admin\CategoryController::class, 'edit']);
-    Route::put('update-category/{category_id}', [App\Http\Controllers\Admin\CategoryController::class, 'update']);
-//    Route::get('delete-category/{category_id}',[App\Http\Controllers\Admin\CategoryController::class,'destroy']);
-    Route::post('delete-category', [App\Http\Controllers\Admin\CategoryController::class, 'destroy']);
-
-    //posts
-    Route::get('posts', [App\Http\Controllers\Admin\PostController::class, 'index']);
-    Route::get('add-post', [App\Http\Controllers\Admin\PostController::class, 'create']);
-    Route::post('add-post', [App\Http\Controllers\Admin\PostController::class, 'store']);
-    Route::get('post/{post_id}', [App\Http\Controllers\Admin\PostController::class, 'edit']);
-    Route::put('update-post/{post_id}', [App\Http\Controllers\Admin\PostController::class, 'update']);
-    Route::get('delete-post/{post_id}', [App\Http\Controllers\Admin\PostController::class, 'destroy']);
-
-    //users
-    Route::get('users', [App\Http\Controllers\Admin\UserController::class, 'index']);
-    Route::get('user/{user_id}', [App\Http\Controllers\Admin\UserController::class, 'edit']);
-    Route::put('update-user/{user_id}', [App\Http\Controllers\Admin\UserController::class, 'update']);
-
-    Route::get('settings', [App\Http\Controllers\Admin\SettingController::class, 'index']);
-    Route::post('settings', [App\Http\Controllers\Admin\SettingController::class, 'savedata']);
-
+Route::get('/', function () {
+    return view('welcome');
 });
